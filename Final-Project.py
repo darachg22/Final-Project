833dd4f9a72ba01f2f3def03bdaae843b7421a8d
Darach Gorham <darachg22@gmail.com>
2024-04-25 15:50:56 +0100

    Updated Routes.

--- Final-Project.py
+++ Final-Project.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def index():
     return "Welcome to the Sligo Surf Report Web App"
 
+@app.route('/about')
+def about():
+    return 'This is the a web application that shows the surf report for the Sligo area.'
+
+@app.route('/Surf Decider')
+def contact():
+    return 'Contact us for more information.'
+
+@app.route('/Current Surfing Statistics')
+def contact():
+    return 'Contact us for more information.'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
