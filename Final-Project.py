123489b67c88120ce1d5d53d3f1b7f5bb0a1b6fa
Darach Gorham <darachg22@gmail.com>
2024-05-09 12:41:30 +0100

    Beginning to put in current weather forecast.

--- Final-Project.py
+++ Final-Project.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+import requests 
 
 app = Flask(__name__)
 
+#Weather report for App
+def weather_forecast(): 
+
+    url = "https://data.gov.ie/dataset/met-eireann-live-text-forecast-data/resource/8350fd63-f6c3-4977-ad16-1dc35fb0972a"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    forecast = weather_forecast()
+    return render_template("index.html", forecast=forecast)
 
 @app.route('/about')
 def about():
